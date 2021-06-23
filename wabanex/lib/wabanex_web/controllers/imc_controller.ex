f868b5a142a9f021d957f84425d30a9c6392da82
Alan Barros <alan.barros@outlook.com>
2021-06-22 22:01:12 -0300

    Aula 3 - Add GraphQL

--- wabanex/lib/wabanex_web/controllers/imc_controller.ex
+++ wabanex/lib/wabanex_web/controllers/imc_controller.ex
@@ -9,11 +9,9 @@ defmodule WabanexWeb.IMCController do
     |> handle_response(conn)
   end
 
-  defp handle_response({:ok, result}, conn), do:
-    render_response(conn, result, :ok)
+  defp handle_response({:ok, result}, conn), do: render_response(conn, result, :ok)
 
-  defp handle_response({:error, result}, conn), do:
-    render_response(conn, result, :bad_request)
+  defp handle_response({:error, result}, conn), do: render_response(conn, result, :bad_request)
 
   defp render_response(conn, result, status) do
     conn
